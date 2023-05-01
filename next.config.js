6786afe0c4d3e4c7e4c0cdf6e82d59afe0a075bb
DanWttvn <daniela@z1.digital>
2023-05-01 16:57:02 +0200

    fix: env variables

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
     // We don't lint during the build because GitHub actions performs its own lint step
     ignoreDuringBuilds: true,
   },
-};
-
-module.exports = nextConfig;
+  env: {
+    MAILCHIMP_SUBSCRIBE: process.env['MAILCHIMP_SUBSCRIBE'],
+    SENDINBLUE_SUBSCRIBE: process.env['SENDINBLUE_SUBSCRIBE'],
+    SENDINBLUE_JOIN: process.env['SENDINBLUE_JOIN'],
+  },
+}
