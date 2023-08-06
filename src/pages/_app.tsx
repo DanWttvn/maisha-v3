464db0d87d0a0afe0d0030230d707635f844f611
DanWttvn <daniela@z1.digital>
2023-08-06 20:27:41 +0200

    feat: add google tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,28 @@ import themes from 'styles/themes'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { LanguageContextProvider } from 'contexts/language'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const GA_MEASUREMENT_ID = process.env['GA_MEASUREMENT_ID'] || ''
+
   return (
     <ThemeProvider theme={themes.light}>
       <LanguageContextProvider>
         <GlobalStyle />
         <Component {...pageProps} />
+        {/* Google tag manager: https://nextjs.org/docs/messages/next-script-for-ga#possible-ways-to-fix-it */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', "G-${GA_MEASUREMENT_ID}");
+          `}
+        </Script>
       </LanguageContextProvider>
     </ThemeProvider>
   )
